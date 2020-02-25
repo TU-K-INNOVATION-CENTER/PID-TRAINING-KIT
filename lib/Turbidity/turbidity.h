ec51526e077c5093ad218ed2596127048dbc7f4b
Jack Mcleans <jackagwaya@gmail.com>
2020-02-25 16:47:47 +0300

    declaration of turbidity methods

--- lib/Turbidity/turbidity.h
+++ lib/Turbidity/turbidity.h
@@ -8,9 +8,20 @@ namespace tuk_lab{
     class Turbidity{
         private:
         private:
+            int sensor_pin;
+            int sensorValue = 0;
+            float turbidity_volage = 0.00;
+            float turbidity = 0.00;
+            float Vclear = 2.85; // Output turbidity_volage to calibrate (with clear water).
+            int number_of_samples = 25;
+
         public:
         public:
-            Turbidity()
+            Turbidity(int sensor_pin);
+            void initialize();
+            float get_value();
+            void calibrate();
+
     };
 }
 
