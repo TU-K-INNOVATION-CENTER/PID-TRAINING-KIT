e237177164a3b980ba4fc762a16089475e10061c
Jack Mcleans <jackagwaya@gmail.com>
2020-02-25 16:40:21 +0300

    Turbidity header file

---
+++ lib/Turbidity/turbidity.h
@@ -0,0 +1,18 @@
+#ifndef _TURBIDITY_
+#define _TURBIDITY_
+
+#define <Arduino.h>
+
+namespace tuk_lab{
+
+    class Turbidity{
+        private:
+        private:
+        public:
+        public:
+            Turbidity()
+    };
+}
+
+
+#endif //_TURBIDITY_
