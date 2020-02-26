14f756b96eb694bc663ed6a2db1eb2e2b4eaca83
MCLEANS <jackagwaya@gmail.com>
2020-02-26 14:18:27 +0300

    removal of turbidity implemantation

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,8 @@ tuk_lab::Turbidity turbidity_sensor(turbidity_sensor_pin);
 void setup() {
   //start serial port at baudrate 9600bits/sec
   Serial.begin(9600);
-    pinMode(calibration_button_pin, INPUT);
-    digitalWrite(calibration_button_pin,HIGH);
+  pinMode(calibration_button_pin, INPUT);
+  digitalWrite(calibration_button_pin,HIGH);
   turbidity_sensor.initialize();
 
 }
@@ -23,14 +23,7 @@ void loop() {
 
   uint8_t calibration_push_button = digitalRead(calibration_button_pin);
 
-  if (calibration_push_button == HIGH) {
-
-    float turbidity_value = turbidity_sensor.get_value();
-  }
-  else{
-    turbidity_sensor.calibrate();
-  }
-  
+ 
 
 
 }
