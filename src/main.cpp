f5d081d56c89ca306f5205bc450d8ccf5a01d009
MCLEANS <jackagwaya@gmail.com>
2020-03-12 15:54:33 +0300

    PID control

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,60 @@
 #define ph_sensor_pin A2
 #define calibration_button_pin 2
 
+#define acid_pump 5;
+#define base_pump 6;
+
 
 tuk_lab::Turbidity turbidity_sensor(turbidity_sensor_pin);
 tuk_lab::Ph_sensor ph_sensor(ph_sensor_pin);
 
 const float temperature = 25;
+
+//--------PID variables--------------------------------------
+double Kp = 5;
+double Ki = 10;
+double Kd = 20;
+
+double sample_period = 200;
+unsigned long last_time;
+double total_error = 0;
+double last_error = 0;
+bool error_type = true; //false is negative error true is positive error
+double control_signal;
+double actual_ph_value = 0;
+double desired_ph_value = 16;
+double error;
+//---------------------------------------------------------------------------
+
+void pid_control() {
+  unsigned long current_time = millis();
+  unsigned long delta_time = current_time - last_time;
+
+  if (delta_time >= sample_period) {
+    error = desired_ph_value - actual_ph_value;
+   // if((error) == 0.00) total_error = 0;
+  //  else  total_error += error;
+    
+    if (total_error >= 255) total_error = 255;
+    if (total_error <= 0) total_error = 0;
+
+    //check if error is positive or negative;
+    if (error < 0) error_type = false;
+    else if (error > 0) error_type = true;
+    //-------------------------------------
+    double delta_error = error - last_error;
+    last_error = error;
+
+
+    control_signal = (Kp * error) + (Ki * total_error * sample_period) + ((Kd / sample_period) * delta_error);
+    if (control_signal >= 255) control_signal = 255;
+    if (control_signal <= -255) control_signal = -255;
+
+
+    last_time = current_time;
+
+  }
+}
 
 void setup() {
   //start serial port at baudrate 9600bits/sec
@@ -45,8 +94,15 @@ void loop() {
     Serial.print(ph_sensor.ph_value,2);
     Serial.print("   Turbidity : ");
     Serial.println(turbidity_value);
-   
 
+    actual_ph_value = ph_sensor.ph_value;
+   
+      pid_control();
+     Serial.print(error);
+      Serial.print(",");
+      Serial.print(control_signal);
+      Serial.print(",");
+      Serial.println(actual_ph_value);
  
 
 
